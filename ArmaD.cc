18d47bb053f41c66cd73c2abf620b5f5f031ad20
EddyTheCo <eddyglezz95@gmail.com>
2020-02-01 16:31:20 +0100

    is implemented for the momentum eigenstates, still no way to test the results

--- ArmaD.cc
+++ ArmaD.cc
@@ -19,32 +19,107 @@ using namespace arma;
 
 
 vector<int> BasisMat;
+vector<size_t> Rperiodicity;
 int Z2_index;
+int Z2_state=0;
 int NumberOfstatesLessHalfChain=2;
-int createZ2(const int L)
-{
- int var=0;
-    for(int i=0;i<L-1;i++)
-    {
-        var+=((i%2)?0:1)*static_cast<int>(pow(2,i));
-    }
 
-    return var;
+size_t  L;
+size_t kmomentum;
+int refRotate=0;
+int leftRotate(int n, unsigned int d)
+{
+    return ((n << d)|(n >> (L - d)))&(refRotate);
 }
 
-void CreateBasis(const int L);
+int reflect(int number)
+{
+    int reflect=0;
+    for(size_t j=0;j<L;j++)
+    {
+        if(number&(1<<j))reflect=reflect|(1<<(L-1-j));
+    }
+
+    return reflect;
+}
+void createZ2()
+{
+
+    for(size_t i=0;i<L-1;i++)
+    {
+        Z2_state+=((i%2)?0:1)*static_cast<int>(pow(2,i));
+    }
+
+
+}
+
+
+
+void checkstate(const int &number)
+{
+
+    int shift=number;
+    for(size_t j=0;j<L;j++)
+    {
+        shift=leftRotate(shift,1);
+
+        if(shift<number)
+        {
+            return;
+        }
+        else
+        {
+            if(shift==number)
+            {
+                if((kmomentum%L/(j+1))!=0)return;
+                BasisMat.push_back(number);
+                Rperiodicity.push_back(j+1);
+
+                return;
+            }
+        }
+
+    }
+}
+void CreateBasis()
+{
+    createZ2();
+    for(size_t i=0;i<L;i++)
+    {
+        refRotate+=pow(2,i);
+    }
+
+    for (int i = 0; i < pow(2,L); i++)
+    {
+
+        if(!(i&(leftRotate(i,1))))
+        {
+            checkstate(i);
+        }
+
+    }
+
+
+}
+
 
 int main()
 {
 
-    int  L;
-    cout<<"Enter L"<<endl;
-    cin>>L;
+
+    cout<<"Enter L and k"<<endl;
+    cin>>L>>kmomentum;
 
 
 
     auto start0 = chrono::high_resolution_clock::now();
-    CreateBasis(L);
+    CreateBasis();
+/*cout<<"*************"<<endl;
+    for (size_t i=0;i<BasisMat.size();i++)
+    {
+        cout<<BasisMat.at(i)<<endl;
+    }
+cout<<"*************"<<endl;*/
 
     auto start1 = chrono::high_resolution_clock::now();
     cout<<" Elapsed time Creating the Basis " << chrono::duration<double>(start1 - start0).count()<<"s"<<endl;
@@ -52,18 +127,28 @@ int main()
 
     const size_t BASISSIZE=BasisMat.size();
     cout<<"SIZE OF THE BASIS:"<<BASISSIZE<<endl;
-    mat Hamil(BASISSIZE,BASISSIZE);
+    mat Hamil(BASISSIZE,BASISSIZE,fill::zeros);
     for(size_t i=0;i<BASISSIZE;i++)
     {
         for(size_t j=0;j<i;j++)
         {
 
-            int var=(BasisMat.at(i))^(BasisMat.at(j));
-            if(__builtin_popcount(var)==1)
+            int lefSta=BasisMat.at(i);
+
+            double Hm=0;
+            for(size_t l=0;l<L;l++)
             {
-                Hamil.at(i,j)=1.;
-                Hamil.at(j,i)=1.;
+                lefSta=leftRotate(lefSta,1);
+                if(__builtin_popcount(lefSta^BasisMat.at(j))==1)
+                {
+
+                    Hm+=sqrt(Rperiodicity.at(j)/Rperiodicity.at(i));
+
+                }
+
             }
+            Hamil(i,j)=Hm;
+            Hamil(j,i)=Hm;
 
         }
     }
@@ -94,17 +179,19 @@ int main()
     TH2D* Prof=new TH2D("E_Vs_Log10(overlap)","E_Vs_Log10(overlap)",1000,-20.,20.,1000,-10.,0.);
       TH2D* Prof2=new TH2D("E_Vs_Z1","E_Vs_Z1",1000,-20.,20.,1000,-0.63,-0.27);
       TH2D* Prof3=new TH2D("E_Vs_S","E_Vs_S",1000,-20.,20.,1000,0.,7.);
+      cout<<"out of E   Z1"<<endl;
       for(size_t j=0;j<eigval.size();j++)
       {
 
-          const double val=pow(eigvec.at(Z2_index,j),2);
+          const double val=pow(eigvec.at(BASISSIZE-1,j),2);
           double val2=0;
-          double val3=0;
+          //double val3=0;
           for(size_t k=0;k<BASISSIZE;k++)
           {
-              val2+=(__builtin_popcount(BasisMat.at(k))- (L*1.-__builtin_popcount(BasisMat.at(k))))/L*pow(eigvec.at(k,j),2);
+              val2+=(__builtin_popcount(BasisMat.at(k))- (L*1.-__builtin_popcount(BasisMat.at(k))))/L*norm(eigvec.at(k,j));
           }
-          mat B = reshape(eigvec.col(j), NumberOfstatesLessHalfChain,BASISSIZE/NumberOfstatesLessHalfChain);
+            cout<<real(eigval(j))<<" "<<val2<<endl;
+         /* mat B = reshape(eigvec.col(j), NumberOfstatesLessHalfChain,BASISSIZE/NumberOfstatesLessHalfChain);
           mat Reduced=B*B.t();
 
           vec eigval2;
@@ -117,10 +204,10 @@ int main()
               const double elem=eigval2.at(h);
               if(elem>0.0000000000001)val3+=elem*log(elem);
           }
-
-          Prof->Fill(eigval(j),log10(val));
-          Prof2->Fill(eigval(j),val2);
-          Prof3->Fill(eigval(j),-val3);
+            */
+          Prof->Fill(real(eigval(j)),log10(val));
+          Prof2->Fill(real(eigval(j)),val2);
+          //Prof3->Fill(eigval(j),-val3);
 
 
       }
@@ -134,53 +221,6 @@ int main()
 
 }
 
-void recur(const int &number, const int &L ,const int &k,const int &Z_2)
-{
-    static bool var=false;
-    if(k>=L-1)return;
-    for(size_t m=(2+k);m<L-1;m++)
-    {
-        const int number2=number|(1<<(m));
 
-        if(number2>pow(2,L/2))
-        {
-            BasisMat.push_back(number2);
-            if(number2==Z_2)
-            {
-                var=true;
-                Z2_index=BasisMat.size()-1;
-
-            }
-        }
-        else
-        {
-            NumberOfstatesLessHalfChain++;
-            BasisMat.insert(BasisMat.begin(),number2);
-            if(var)
-            {
-                Z2_index++;
-            }
-        }
-
-
-
-
-        recur(number2,L,m,Z_2);
-    }
-
-
-
-
-}
-void CreateBasis(const int L)
-{
-    int Z_2=createZ2(L);
-    BasisMat.push_back(0);
-    BasisMat.push_back(1);
-
-        recur(1,L,0,Z_2);
-
-
-}
 
 
